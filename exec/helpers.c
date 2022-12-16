43f17ce01a7c537e93ff06c30a6715f433acc019
Khaoula Adjane <kadjane@e2r10p3.1337.ma>
2022-12-16 15:40:51 +0100

    v2

--- exec/helpers.c
+++ exec/helpers.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   helpers.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ntaleb <ntaleb@student.42.fr>              +#+  +:+       +#+        */
+/*   By: kadjane <kadjane@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 18:51:32 by ntaleb            #+#    #+#             */
-/*   Updated: 2022/12/16 12:42:15 by ntaleb           ###   ########.fr       */
+/*   Updated: 2022/12/16 15:22:52 by kadjane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,7 +63,7 @@ int	get_append_flag(struct s_list_io_stream *io)
 	if (io->flags == TOKEN_FILE_APPAND)
 		return (O_APPEND);
 	else
-		return (0);
+		return (O_TRUNC);
 }
 
 int	arr_size(char **arr)
