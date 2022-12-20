1d4f38bdd3c22b9aa13b69b933c07b4b83cd16d4
Khaoula Adjane <kadjane@e2r8p1.1337.ma>
2022-12-20 13:34:28 +0100

    gnl handle interrupts && handle recursive minishells

--- exec/signal.c
+++ exec/signal.c
@@ -3,15 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   signal.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ntaleb <ntaleb@student.42.fr>              +#+  +:+       +#+        */
+/*   By: kadjane <kadjane@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 18:50:59 by ntaleb            #+#    #+#             */
-/*   Updated: 2022/12/19 21:42:30 by ntaleb           ###   ########.fr       */
+/*   Updated: 2022/12/20 13:29:32 by kadjane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
-#include <signal.h>
 
 /**
  * rl_on_new_line: behave like we have a new line
@@ -21,9 +20,9 @@
 void	sigint_handler(int sig)
 {
 	(void)sig;
-	printf("\n");
 	if (g_state.readline_done)
 		return ;
+	printf("\n");
 	rl_on_new_line();
 	rl_replace_line("", 1);
 	rl_redisplay();
